24c90ba3eba187ee45f71971f0adf7fb540362de
Jeiman Cabarcas <jeiman@wordlist.com>
2026-06-26 14:26:11 -0500

    refactor: remove app-dashboard-layout component and update test to render app component

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -14,10 +14,10 @@ describe('App', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should render router outlet', async () => {
+  it('should render app component', async () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('router-outlet')).toBeTruthy();
+    expect(compiled.querySelector('app-root')).toBeTruthy();
   });
 });
